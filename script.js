fd5c154336f4d1be1aa257420a5461c7a5df245b
VWaghray <87341415+VWaghray@users.noreply.github.com>
2022-01-30 14:16:44 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+function startClassification() {
+    navigator.mediaDevices.getUserMedia({
+        audio: true
+    });
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/6Iqh94pP9/model.json', modelReady);
+}
+
+function modelReady() {
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+
+        document.getElementById("result_label").innerHTML = 'I can hear - ' + results[0].label;
+        document.getElementById("result_confidence").innerHTML = 'Accuracy - ' + (results[0].confidence * 100).toFixed(2) + " %";
+        document.getElementById("result_label").style.color = "rgb(" + random_number_r + "," + random_number_g + "," + random_number_b + ")";
+        document.getElementById("result_confidence").style.color = "rgb(" + random_number_r + "," + random_number_g + "," + random_number_b + ")";
+
+        img = document.getElementById('Dog');
+        img1 = document.getElementById('Cat');
+        img2 = document.getElementById('Lion');
+        img3 = document.getElementById('Cow');
+
+        if (results[0].label == "Barking") {
+            img.src = 'Dog Jif.gif'
+            img1.src = 'Cute Cat.jpg'
+            img2.src = 'Lion.png'
+            img3.src = 'Cow.jpg'
+        } else if (result[0].label == "Meowing") {
+            img.src = 'Dogs.webp'
+            img1.src = 'Cute Cat Jif.gif'
+            img2.src = 'Lion.png'
+            img3.src = 'Cow.jpg'
+        } else if (result[0].label == "Roaring") {
+            img.src = 'Dogs.webp'
+            img1.src = 'Cute Cat.jpg'
+            img2.src = 'Lion Jif.gif'
+            img3.src = 'Cow.jpg'
+        } else {
+            img.src = 'Dogs.webp'
+            img1.src = 'Cute Cat.jpg'
+            img2.src = 'Lion.png'
+            img3.src = 'Cow Jif.gif'
+        }
+    }
+}
